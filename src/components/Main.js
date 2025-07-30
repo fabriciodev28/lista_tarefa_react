0a5942f7e4b8fcd8197249c67e0ade8bd6ededc0
fabriciodev28 <fabriciodev28@gmail.com>
2025-07-30 13:09:41 -0300

    Editando layout de tarefas

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,11 +3,18 @@ import React, { Component } from "react"
 //Form
 import { FaPlus } from "react-icons/fa"
 
+//Form
+import { FaEdit, FaWindowClose } from "react-icons/fa"
+
 import './Main.css'
 
 class Main extends Component {
     state = {
         novaTarefa: '',
+        tarefas: [
+            'Estudar',
+            'Fazer exercício',
+        ]
     }
     
     handleChange = (e) =>{
@@ -20,7 +27,7 @@ class Main extends Component {
 
     render(){
         
-        const {novaTarefa} = this.state
+        const {novaTarefa, tarefas} = this.state
 
         return(
             <div className="main">
@@ -30,6 +37,19 @@ class Main extends Component {
                     <input onChange={this.handleChange} type="text"></input>
                     <button type="submit"><FaPlus/></button>
                 </form>
+
+                <ul className="tarefas">
+                    {tarefas.map((tarefa)=>(
+                        <li key={tarefa}>
+                            {tarefa}
+                            <div>
+                                <FaEdit className="edit"/>
+                                <FaWindowClose className="delete"/>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+
             </div>
         )
     }
