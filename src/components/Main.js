5d58d0ae02fa696c3d911454a31ba431cb9cfcb5
fabriciodev28 <fabriciodev28@gmail.com>
2025-07-31 12:49:03 -0300

    Criado logica para adicionar tarefas

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,11 +11,25 @@ import './Main.css'
 class Main extends Component {
     state = {
         novaTarefa: '',
-        tarefas: [
-            'Estudar',
-            'Fazer exercício',
-        ]
+        tarefas: []
     }
+
+
+    handleSubmit = (e) =>{
+        e.preventDefault()
+        const { tarefas } = this.state
+        let { novaTarefa } = this.state
+        novaTarefa = novaTarefa.trim()
+
+        if(tarefas.indexOf(novaTarefa) !== -1) return
+
+        const novasTarefas = [...tarefas]
+
+        this.setState({
+            tarefas: [...novasTarefas, novaTarefa]
+        })
+    }
+
     
     handleChange = (e) =>{
         this.setState({
@@ -33,7 +47,7 @@ class Main extends Component {
             <div className="main">
                 <h1>Lista de Tarefas</h1>
 
-                <form className="form">
+                <form className="form" onSubmit={this.handleSubmit}>
                     <input onChange={this.handleChange} type="text"></input>
                     <button type="submit"><FaPlus/></button>
                 </form>
@@ -42,10 +56,10 @@ class Main extends Component {
                     {tarefas.map((tarefa)=>(
                         <li key={tarefa}>
                             {tarefa}
-                            <div>
+                            <span>
                                 <FaEdit className="edit"/>
                                 <FaWindowClose className="delete"/>
-                            </div>
+                            </span>
                         </li>
                     ))}
                 </ul>
